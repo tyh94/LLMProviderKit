697cab7d15b7172ef3b60173a821554cbcac24e8
Макеева Татьяна <tyh94@mail.ru>
2026-06-23 14:30:40 +0700

    Update Storage

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/tyh94/Storage.git", from: "1.0.1"),
+        .package(url: "https://github.com/tyh94/Storage.git", from: "1.0.2"),
     ],
     targets: [
         .target(
